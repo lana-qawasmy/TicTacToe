cfee169f9cc7947e9b47804c17c323988e32a6cc
Momen-G-Ar-Official <cp.momen@gmail.com>
2022-10-14 10:31:32 +0300

    Complete the check function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/board/board.component';
 import Score from './components/score/score.component';
 
 function App() {
+  const [winner, setWinner] = useState('');
+  const onWin = (win) => {
+    setWinner = win;
+  }
   return (
     <div className="App">
       <Score />
-      <Board />
+      <Board
+        onWin = {onWin(winner)}
+      />
     </div>
   );
 }
--- src/components/board/board.component.jsx
+++ src/components/board/board.component.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './board.css'
 
-const Board = () => {
+const Board = (props) => {
 
     const [cells, setCells] = useState(Array(9).fill(""));
     const [turn, setTurn] = useState('X');
@@ -12,8 +12,66 @@ const Board = () => {
 
         const arr = [...cells];
         arr[index] = turn;
-        setTurn(turn === 'X'? 'O' : 'X');
-        setCells([...arr]);
+        setTurn(turn === 'X' ? 'O' : 'X');
+        setCells(arr);
+
+        let winner = checkWinner(arr);
+        if(winner !== '')
+            props.onWin(winner);
+    }
+
+    const checkWinner = (arr) => {
+        // Check the rows 
+        for (let j = 0; j < 3; j++) {
+            let X = 0, O = 0;
+            for (let i = 0; i < 3; i++) {
+                X += (arr[3 * j + i] === 'X');
+                O += (arr[3 * j + i] === 'O');
+            }
+            if (X === 3)
+                return 'X';
+            else if (O === 3)
+                return 'O';
+        }
+        // Check the columns
+        for (let j = 0; j < 3; j++) {
+            let X = 0, O = 0;
+            for (let i = 0; i < 3; i++) {
+                X += (arr[j + 3 * i] === 'X');
+                O += (arr[j + 3 * i] === 'O');
+            }
+            if (X === 3)
+                return 'X';
+            else if (O === 3)
+                return 'O';
+        }
+        // Check the main diagonal 
+        let X = 0, O = 0;
+        for (let i = 0; i <= 8; i += 4) {
+            X += (arr[i] === 'X');
+            O += (arr[i] === 'O');
+        }
+        if (X === 3)
+            return 'X';
+        else if (O === 3)
+            return 'O';
+        // Check the reverse diagonal
+        X = O = 0;
+        for (let i = 2; i <= 6; i += 2) {
+            X += (arr[i] === 'X');
+            O += (arr[i] === 'O');
+        }
+        if (X === 3)
+            return 'X';
+        else if (O === 3)
+            return 'O';
+
+        // Check the Draw
+        let a = arr.filter((item) => item === '');
+        if(a.length)
+            return '';
+        else
+            return 'Draw';
     }
 
     return (
@@ -27,6 +85,7 @@ const Board = () => {
             }
         </div>
     );
+
 }
 
 export default Board;
