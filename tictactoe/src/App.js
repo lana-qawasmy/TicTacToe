6e973cde6894f6a1c036a93c6b99b60ac23d6b19
raghad__ns <raghadnadersalem20022017@gmail.com>
2022-10-20 17:05:17 +0300

    problem need to be fixed in evaluation function

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -5,10 +5,20 @@ import { useState } from 'react';
 
 function App() {
   const [turn , setTurn] = useState ('X') ;
+  const [winner , setWinner] = useState ({xWin : 0 , oWin : 0}) ;
   const flipTurn = (newTurn) => setTurn (newTurn);
+  const addWin = (newWineer) => {
+    const temp = {...winner} ;
+    if (newWineer === 'X')
+      temp.xWin ++ ;
+    if (newWineer === 'O')
+      temp.oWin ++ ;
+    setWinner (temp) ;
+    console.log ('winner' , temp) ;
+  }
   return (
     <div className="App">
-      <Board turn = {turn}  onChange = {flipTurn}/>
+      <Board turn = {turn} winner = {winner} onWin = {addWin}  onChange = {flipTurn}/>
     </div>
   );
 }
--- tictactoe/src/components/board/board.component.jsx
+++ tictactoe/src/components/board/board.component.jsx
@@ -13,14 +13,44 @@ const Board = (props) => {
     //         setContent (newContent) ;
     //         console.log ('content' , content) ;
     //     }
-    // } 
+    // }
+    const [winner , setWinner] = useState ('none') ;
+    const evaluate = () => {
+        
+        if (content[0] === content[1] && content[0] === content[2] && content[0] !== '')
+            {setWinner (content[0]) ;}
+        if (content[3] === content[4] && content[3] === content[5] && content[3] !== '')
+            {setWinner (content[3]) ;}
+        if (content[6] === content[7] && content[6] === content[8] && content[6] !== '')
+            {setWinner (content[6]) ;}
+        if (content[0] === content[3] && content[0] === content[6] && content[0] !== '')
+            {setWinner (content[0]) ;}
+        if (content[1] === content[4] && content[1] === content[7] && content[1] !== '')
+            {setWinner (content[1]) ;}
+        if (content[2] === content[5] && content[2] === content[8] && content[2] !== '')
+            {setWinner (content[3]) ;}
+        if (content[0] === content[4] && content[0] === content[8] && content[0] !== '')
+            {setWinner (content[0]) ;}
+        if (content[2] === content[4] && content[2] === content[6] && content[2] !== '')
+            {setWinner (content[2]) ;}
+        const thereIsEmptyCells = false ;
+        content.forEach(element => {
+            if (element === '')
+                thereIsEmptyCells = true ;
+        });
+        setWinner (thereIsEmptyCells ? '' : 'draw') ;
+        console.log ('current winner' , winner) ;
+        return winner ;
+    } 
     
   return (
     <div>
         <div className="infoBoard">
-            <span></span>
-            <span></span>
-            <span></span>
+            <span>X wins : {props.winner.xWin}</span>
+            <span>| </span>
+            <span>O wins : {props.winner.oWin}</span>
+            <span>| </span>
+            <span>Turn : {props.turn}</span>
         </div>
         <div className="boardContent">
             {
@@ -33,14 +63,18 @@ const Board = (props) => {
                         props.onChange (props.turn === 'X' ? 'O' : 'X') ;
                         setContent (newContent) ;
                         console.log ('content' , content) ;
+                        props.onWin (() => evaluate) ;
                     }
+                    // console.log ('current winner' , winner) ;
                 } }
+                className = {item == '' ? '' : 'filled'} 
                 >{item} </button>
                 )
             }
+            {evaluate}
         </div>
     </div>
   )
 }
 
-export default Board
+export default Board;
