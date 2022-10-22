513b9adf86a6b85381790fc2d16a2dd141a6da9a
Rami Ikhmayyes <191003@ppu.edu.ps>
2022-10-22 20:37:36 +0300

    Further Styling

--- xo/src/components/common/reset.component.jsx
+++ xo/src/components/common/reset.component.jsx
@@ -4,6 +4,6 @@ import "./reset.css";
 
 export const Reset = ({ resetBoard }) => {
     return (
-        <button className="reset-btn" onClick={resetBoard}>Reset</button>
+        <button className="reset-btn" onClick={resetBoard}>Reset !</button>
     )
 }
